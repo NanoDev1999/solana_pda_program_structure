7bb0e3cb5b4cf2124d5c434ad511e6a2549f52b8
jekdev1999 <jekdev1999@gmail.com>
2024-02-09 04:15:39 -0500

    comment states added

--- src/state.rs
+++ src/state.rs
@@ -5,15 +5,19 @@ use solana_program::{
 
 use borsh::{BorshSerialize, BorshDeserialize};
 
+
+
+
+
 #[derive(BorshSerialize, BorshDeserialize)]
 pub struct MovieAccountState {
+    pub discriminator: String,
     pub is_initialized: bool,
+    pub reviewer: Pubkey,
     pub rating: u8,
     pub title: String,
     pub description: String,
-    pub reviewer: Pubkey,
 }
-
 
 impl Sealed for MovieAccountState {}    // this specifies that MoveAccountState has a known size. Solana's version of Sized in Rust.
 
@@ -23,3 +27,70 @@ impl IsInitialized for MovieAccountState {
     }
 }
 
+impl MovieAccountState {
+    pub const DISCRIMINATOR: &'static str = "review";
+
+    pub fn get_account_size(title: String, description: String) -> usize {
+        return (4 + MovieAccountState::DISCRIMINATOR.len())
+            + 1
+            + 1
+            + (4 + title.len())
+            + (4 + description.len());
+    }
+}
+
+
+
+
+
+
+
+#[derive(BorshSerialize, BorshDeserialize)]
+pub struct MovieCommentCounter {
+    pub discriminator: String,
+    pub is_initialized: bool,
+    pub counter: u64
+}
+
+impl IsInitialized for MovieCommentCounter {
+    fn is_initialized(&self) -> bool {
+        self.is_initialized
+    }
+}
+
+impl MovieCommentCounter {
+    pub const DISCRIMINATOR: &'static str = "counter";
+    pub const SIZE: usize = (4 + MovieCommentCounter::DISCRIMINATOR.len()) + 1 + 8;
+}
+
+
+
+
+#[derive(BorshSerialize, BorshDeserialize)]
+pub struct MovieComment {
+    pub discriminator: String,
+    pub is_initialized: bool,
+    pub review: Pubkey,
+    pub commenter: Pubkey,
+    pub comment: String,
+    pub count: u64
+}
+
+impl IsInitialized for MovieComment {
+    fn is_initialized(&self) -> bool {
+        self.is_initialized
+    }
+}
+
+impl MovieComment {
+    pub const DISCRIMINATOR: &'static str = "comment";
+
+    pub fn get_account_size(comment: String) -> usize {
+        return (4 + MovieComment::DISCRIMINATOR.len()) + 1 + 32 + 32 + (4 + comment.len()) + 8;
+    }
+}
+
+
+
+
+ 
